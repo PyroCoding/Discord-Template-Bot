cfbf2be298abdabda71142592ca2e616b65adda0
lucaslulu2012 <lucaslulu2012@users.noreply.github.com>
2020-02-24 13:14:55 -0600

    Update and rename placeholder.js to commandtemplate.js

--- commands/commandtemplate.js
+++ commands/commandtemplate.js
@@ -1,9 +1,7 @@
 const Discord = module.require('discord.js')
 
 module.exports.run = async(bot, message, args) => {
-	message.channel.send('This is a place holder command, please delete.')
 }
 module.exports.help = {
-name: "placeholder",
-description: "holds a place"
+name: "CommandTemplate",
 }
